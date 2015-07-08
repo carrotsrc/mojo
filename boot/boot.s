f820cc3e1658666fcfc39352e1ccdf65274e2816
Charlie FG <cfg@zunautica.org>
2015-07-08 18:40:22 +0100

    Remove the static gdt descriptors from boot.s

--- boot/boot.s
+++ boot/boot.s
@@ -19,60 +19,32 @@ stack_bottom:
 .skip 16384
 stack_top:
 
-.global __pp_gdt
 .global start
+.extern _gp
 
 start:
-	hlt
-	jmp start
-
-	lgdt __pp_gdt
-	ljmp $0x08, $start_hi
-	
-start_hi:
-	mov	$0x10, %eax		# we reload the segments here
-	mov	%eax, %ds
-	mov	%eax, %es
-	mov	%eax, %fs
-	mov	%eax, %gs
-	mov	%eax, %ss
-
 	movl	$stack_top, %esp # mov the stack into the stack pointer register (32bit)
 	mov	0x44, %eax
 	push	%ebx
-	call	kmain # run the kernel code
+	#call	kmain # run the kernel code
 
 	# here we have returned from kernel_main
 hang:
 	hlt
 	jmp	hang
 
-# Raw global descriptor table
-.section .ppinit
+.global gdt_reload
 
-__pp_gdt:
-	.short __pp_gdt_end - __pp_gdt_gates -1
-	.long __pp_gdt_gates
+gdt_reload:
+	cli
+	lgdt	_gp			# reload the gdt pointer
+	ljmp 	$0x08,$gdt_rld_seg	# far jump to [codeseg:gdt_rld_seg]
 
-__pp_gdt_gates:
-	.byte 0x00, 0x00	# limit
-	.byte 0x00, 0x00	# base lo
-	.byte 0x00		# base mid
-	.byte 0x00		# access
-	.byte 0x00		# granularity
-	.byte 0x00		# base hi
-
-	.byte 0xFF, 0xFF	# limit
-	.byte 0x00, 0x00	# base lo
-	.byte 0x00		# base mid
-	.byte 0x9a		# access
-	.byte 0xcf		# granularity
-	.byte 0x40		# base hi
-
-	.byte 0xFF, 0xFF	# limit
-	.byte 0x00, 0x00	# base lo
-	.byte 0x00		# base mid
-	.byte 0x92		# access
-	.byte 0xcf		# granularity
-	.byte 0x40		# base hi
-__pp_gdt_end:
+gdt_rld_seg:
+	mov	$0x10, %ax		# we reload the segments here
+	mov	%eax, %ds
+	mov	%eax, %es
+	mov	%eax, %fs
+	mov	%eax, %gs
+	mov	%eax, %ss
+	ret
